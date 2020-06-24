d2666b339ec6b314f79b0977629937a88053a2d8
hishambokhari <hish.b92@gmail.com>
2020-06-25 00:40:14 +0500

    hooks

--- src/App.js
+++ src/App.js
@@ -1,41 +1,58 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person.js';
 
 
-class App extends Component {
-
-  state = { // managing component data from within the component
+const app = props => {
+  const [ personsState, setPersonsState ] = useState({ // managing component data from within the component
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Tom', age: 15 },
       { name: 'Joe', age: 32 }
-    ]
-  }
+    ],
+    otherState: 'some other value'
+  });
 
-  switchNameHandler = () => {
+  const switchNameHandler = () => {
     // console.log('was clicked');
-    this.setState({persons: [
+    setPersonsState({
+      persons: [
       { name: 'Mahak', age: 28 },
       { name: 'Hisham', age: 15 },
       { name: 'Sayem', age: 32 }
     ]
     })
-  }
+  };
 
-  render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>  
         <p>Thi is working too</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age= {this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age= {this.state.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age= {this.state.persons[2].age}/>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age= {personsState.persons[0].age}/>
+        <Person name={personsState.persons[1].name} age= {personsState.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={personsState.persons[2].name} age= {personsState.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
-  }
 }
 
-export default App;
+export default app;
+
+
+// state = { // managing component data from within the component
+//   persons: [
+//     { name: 'Max', age: 28 },
+//     { name: 'Tom', age: 15 },
+//     { name: 'Joe', age: 32 }
+//   ]
+// }
+
+// switchNameHandler = () => {
+//   // console.log('was clicked');
+//   this.setState({persons: [
+//     { name: 'Mahak', age: 28 },
+//     { name: 'Hisham', age: 15 },
+//     { name: 'Sayem', age: 32 }
+//   ]
+//   })
