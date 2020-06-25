a2c2597f52ec1a778e57e06be4e1e0741d7c43b8
hishambokhari <hish.b92@gmail.com>
2020-06-25 13:02:26 +0500

    bind

--- src/App.js
+++ src/App.js
@@ -1,43 +1,52 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person.js';
 
 
-const app = props => {
-  const [ personsState, setPersonsState ] = useState({ // managing component data from within the component
+class App extends Component { // stateful component - manages state
+  state = { // managing component data from within the component
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Tom', age: 15 },
       { name: 'Joe', age: 32 }
     ],
     otherState: 'some other value'
-  });
+  };
 
-  const switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('was clicked');
-    setPersonsState({
+    this.setState({
       persons: [
-      { name: 'Mahak', age: 28 },
+      { name: newName, age: 28 },
       { name: 'Hisham', age: 15 },
       { name: 'Sayem', age: 32 }
     ]
     })
   };
-
+    render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>  
         <p>Thi is working too</p>
-        <button onClick={switchNameHandler}>Switch Name</button>
-        <Person name={personsState.persons[0].name} age= {personsState.persons[0].age}/>
-        <Person name={personsState.persons[1].name} age= {personsState.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={personsState.persons[2].name} age= {personsState.persons[2].age}/>
+        <button onClick={this.switchNameHandler.bind(this, 'Mahak')}>Switch Name</button>
+        <Person 
+        name={this.state.persons[0].name} 
+        age= {this.state.persons[0].age}/>
+        <Person 
+        name={this.state.persons[1].name} 
+        age= {this.state.persons[1].age}
+        click={this.switchNameHandler.bind(this, 'Mahak!!!')}>My Hobbies: Racing</Person>
+        <Person 
+        name={this.state.persons[2].name} 
+        age= {this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
-}
+    }
+  }
 
-export default app;
+
+export default App;
 
 
 // state = { // managing component data from within the component
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 // props is the attributes you add to your component
 
-const person = (props) => { //es6 equiv for functions
+const person = (props) => { //es6 equiv for functions - this is a stateless component - has no internal state management
  
   return (
     <div>
-      <p>I'm {props.name} and I am {props.age} years old!</p>
+      <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
       <p>{props.children}</p>
     </div>
   )
