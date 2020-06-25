3e187ee69cd50caa1e0c353b6ec669c126120324
hishambokhari <hish.b92@gmail.com>
2020-06-25 14:32:18 +0500

    inline styling

--- src/App.js
+++ src/App.js
@@ -35,11 +35,20 @@ class App extends Component { // stateful component - manages state
   }
 
     render() {
+      const style = {
+        backgroundColor: 'white',
+        font: 'inherit',
+        border: '1px solid blue',
+        padding: '8px',
+        cursor: 'pointer'
+      }
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>  
         <p>Thi is working too</p>
-        <button onClick={this.switchNameHandler.bind(this, 'Mahak')}>Switch Name</button>
+        <button 
+        style={style}
+        onClick={this.switchNameHandler.bind(this, 'Mahak')}>Switch Name</button>
         <Person 
         name={this.state.persons[0].name} 
         age= {this.state.persons[0].age}/>
