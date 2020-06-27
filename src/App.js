fa29cbca8c7154d091c6cb2c703aefd8bbb0aec5
hishambokhari <hish.b92@gmail.com>
2020-06-27 13:55:26 +0500

    StyleRoot via Radium

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Person from './Person/Person.js';
-import Radium from 'radium';
+import Radium, { StyleRoot } from 'radium';
 
 
 
@@ -100,6 +100,7 @@ class App extends Component { // stateful component - manages state
       }
 
     return (
+      <StyleRoot>
       <div className="App">
         <h1>Hi, I'm a React App</h1>  
         <p className={classes.join(' ')}>This is working too</p>
@@ -108,6 +109,7 @@ class App extends Component { // stateful component - manages state
         onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
+      </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
     }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,9 +5,14 @@ import Radium from 'radium';
 // props is the attributes you add to your component
 
 const person = (props) => { //es6 equiv for functions - this is a stateless component - has no internal state management
- 
+ const style = {
+   '@media (min-width: 500px)': {
+     width: '450px'
+   }
+ };
+
   return (
-    <div className="Person">
+    <div className="Person" style={style}>
       <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name}/>
