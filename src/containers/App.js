2dbbb90f74262c2c6a4daf4abfcb5f3ddadc176c
hishambokhari <hish.b92@gmail.com>
2020-07-01 18:16:31 +0500

    for state changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -30,6 +30,15 @@ class App extends Component { // stateful component - manages state
     console.log('[App.js] componentDidMount');
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
+  }
+
   deletePersonHandler = (personIndex) => {
     // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
